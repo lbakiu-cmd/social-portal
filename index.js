490b2186b1735f360e255e1264d9e0d475e9ea21
lbakiu-cmd <lbakiu@gmail.com>
2026-07-08 09:40:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 
-const prisma = new PrismaClient(); 
+const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
